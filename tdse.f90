6e415ebb0e0f7bc5c360b022f37f540dc1ef2730
Shunsuke A. Sato <shunsuke.sato26@gmail.com>
2026-07-23 11:06:27 +0900

    Updated the code.

--- tdse.f90
+++ tdse.f90
@@ -23,6 +23,7 @@ module global_variables
 ! Numerical parameters kept near the top for easy changes.
   integer,parameter :: cg_max_iter = 600
   integer,parameter :: output_stride = 1
+  integer,parameter :: num_eigenstates = 4
   real(8),parameter :: cg_energy_tol = 1d-11
   real(8),parameter :: cg_residual_tol = 1d-9
 
@@ -44,6 +45,9 @@ module global_variables
 
 ! wavefunction
   complex(8), allocatable :: zpsi(:,:,:)
+! eigenvectors(ix1,ix2,ix3,state_index), with state_index=1 the ground state
+  real(8), allocatable :: eigenvalues(:), eigenstate_residuals(:)
+  complex(8), allocatable :: eigenvectors(:,:,:,:)
 
 ! potentials
   real(8), allocatable :: vpot_1d(:), wpot_1d(:)
@@ -57,8 +61,12 @@ program main
   real(8) :: e0_gs, res_gs, asym_gs
 
   call initialize
-  call initialize_antisymmetric_state
-  call ground_state_cg(e0_gs, res_gs)
+  call compute_lowest_fermion_states(num_eigenstates, eigenvalues, &
+      eigenvectors, eigenstate_residuals)
+  call output_eigenstate_currents(num_eigenstates, eigenvalues, eigenvectors)
+  zpsi = eigenvectors(:,:,:,1)
+  e0_gs = eigenvalues(1)
+  res_gs = eigenstate_residuals(1)
 
   asym_gs = antisymmetry_error(zpsi)
   write(*,'(a,1pe16.8)') 'Ground-state energy        = ', e0_gs
@@ -81,6 +89,9 @@ subroutine initialize
   allocate(vpot(0:nx-1, 0:nx-1, 0:nx-1))
   allocate(wpot(0:nx-1, 0:nx-1, 0:nx-1))
   allocate(tot_pot(0:nx-1, 0:nx-1, 0:nx-1))
+  allocate(eigenvalues(num_eigenstates))
+  allocate(eigenstate_residuals(num_eigenstates))
+  allocate(eigenvectors(0:nx-1,0:nx-1,0:nx-1,num_eigenstates))
 
   call set_potentials
   call check_time_step
@@ -362,102 +373,294 @@ subroutine apply_hamiltonian(psi, hpsi, avec)
 
 end subroutine apply_hamiltonian
 !-------------------------------------------------------
-!> The algorithm of Jiang et al. Phys. Rev. B 68, 165337 (2003) is impleemnted with a sign fix of Eq. (21).
-subroutine ground_state_cg(energy, residual_norm)
+subroutine compute_lowest_fermion_states(nstates, eigenvalues_out, &
+    eigenvectors_out, residuals_out)
   implicit none
-  real(8),intent(out) :: energy, residual_norm
-  complex(8),allocatable :: hpsi(:,:,:)
-  complex(8),allocatable :: xi(:,:,:), phi_t(:,:,:), phi_old(:,:,:)
-  real(8) :: lambda, xixi, xixi_old, gamma, theta
-  integer :: iter
-  real(8) :: ss, aa, bb
-  complex(8) :: zs
+  integer,intent(in) :: nstates
+  real(8),intent(out) :: eigenvalues_out(nstates), residuals_out(nstates)
+  complex(8),intent(out) :: eigenvectors_out(0:nx-1,0:nx-1,0:nx-1,nstates)
+  complex(8),allocatable :: psi(:,:,:)
+  integer :: istate
 
-  allocate(hpsi(0:nx-1,0:nx-1,0:nx-1))
-  allocate(xi(0:nx-1,0:nx-1,0:nx-1))
-  allocate(phi_t(0:nx-1,0:nx-1,0:nx-1))
-  allocate(phi_old(0:nx-1,0:nx-1,0:nx-1))
+  if (nx < 4 .and. nstates >= 4) then
+    stop 'At least nx=4 is required for four antisymmetric states.'
+  end if
 
-  call antisymmetrize(zpsi)
-  call normalize(zpsi)
-  call apply_hamiltonian(zpsi, hpsi, 0d0)
-  lambda = real(inner_product(zpsi, hpsi))
-
-  xi = lambda*zpsi - hpsi 
-  call antisymmetrize(xi)
-  xixi = real(inner_product(xi, xi))
-  xixi_old = xixi
-
-  residual_norm = xixi
-  energy = lambda
+  allocate(psi(0:nx-1,0:nx-1,0:nx-1))
+  eigenvectors_out = (0d0,0d0)
 
   open(20,file='ground_state.log',status='replace')
   write(20,'(a)') '# iter energy residual_norm norm antisymmetry_error'
-  write(20,'(i8,4(1x,1pe20.12))') 0, energy, residual_norm, &
-      wavefunction_norm(zpsi), antisymmetry_error(zpsi)
+  open(21,file='eigenstates.log',status='replace')
+  write(21,'(a)') '# state iter energy residual_norm norm antisymmetry_error'
 
-  do iter = 0, cg_max_iter
-
-    if(iter == 0)then
-      gamma = 0d0
-      phi_old = 0d0
-    else
-      gamma = xixi/xixi_old
-      xixi_old = xixi
-    end if
-
-    phi_t = xi + gamma*phi_old
-    phi_old = phi_t
-    call antisymmetrize(phi_t)
-
-    zs = inner_product(zpsi, phi_t)
-    phi_t = phi_t - zs*zpsi
-    call antisymmetrize(phi_t)
-    
-    ss = real(inner_product(phi_t, phi_t))
-    phi_t = phi_t/sqrt(ss)
-
-
-    bb = 2d0*real(inner_product(phi_t, hpsi))
-    call apply_hamiltonian(phi_t, hpsi, 0d0)
-    aa = real(inner_product(phi_t, hpsi)) - lambda
-    aa = -aa ! fix: there is a sign error in Eq. (21) of Jiang et al. Phys. Rev. B 68, 165337 (2003)
-
-!    theta = 0.5d0*atan2(bb, aa)
-    if(aa /= 0d0)then
-      theta = 0.5d0*atan(bb/aa)
-    else
-      if(bb > 0d0)then
-        theta = 0.25d0*pi
-      else
-        theta = -0.25d0*pi
-      end if
-    end if
-    zpsi = cos(theta)*zpsi + sin(theta)*phi_t
-    call antisymmetrize(zpsi)
-    call normalize(zpsi)
-
-    if(iter == cg_max_iter)exit
-
-! calc xi
-    call apply_hamiltonian(zpsi, hpsi, 0d0)
-    lambda = real(inner_product(zpsi, hpsi))
-    xi = lambda*zpsi - hpsi
-    call antisymmetrize(xi)
-    xixi = real(inner_product(xi, xi))
-
-    residual_norm = xixi
-    energy = lambda
-
-    write(20,'(i8,4(1x,1pe20.12))') iter+1, energy, residual_norm, &
-        wavefunction_norm(zpsi), antisymmetry_error(zpsi)
-
+  do istate = 1, nstates
+    call initialize_eigenstate_guess(istate, psi)
+    call orthogonalize_against_states(psi, eigenvectors_out, istate-1)
+    call normalize(psi)
+    call minimize_projected_rayleigh(istate, psi, eigenvectors_out, &
+        istate-1, eigenvalues_out(istate), residuals_out(istate))
+    eigenvectors_out(:,:,:,istate) = psi
   end do
 
-
   close(20)
+  close(21)
+  deallocate(psi)
 
-end subroutine ground_state_cg
+  call sort_eigenpairs(nstates, eigenvalues_out, eigenvectors_out, residuals_out)
+  call verify_eigenstates(nstates, eigenvalues_out, eigenvectors_out, residuals_out)
+
+end subroutine compute_lowest_fermion_states
+!-------------------------------------------------------
+subroutine initialize_eigenstate_guess(istate, psi)
+  implicit none
+  integer,intent(in) :: istate
+  complex(8),intent(out) :: psi(0:nx-1,0:nx-1,0:nx-1)
+  integer :: ix1, ix2, ix3, isel
+  integer :: modes(3,4)
+  real(8) :: k(3)
+  complex(8) :: a(3), b(3), c(3)
+
+  if (istate == 1) then
+    call initialize_antisymmetric_state
+    psi = zpsi
+    return
+  end if
+
+! Low-kinetic-energy Slater determinants give reproducible independent seeds.
+  modes(:,1) = (/ -1,  0,  1 /)
+  modes(:,2) = (/ -2, -1,  0 /)
+  modes(:,3) = (/  0,  1,  2 /)
+  modes(:,4) = (/ -2,  0,  1 /)
+  isel = 1 + modulo(istate-1,4)
+  k = 2d0*pi*dble(modes(:,isel))/bvc_lattice_constant
+
+  do ix1 = 0, nx-1
+    do ix2 = 0, nx-1
+      do ix3 = 0, nx-1
+        a = exp(zi*k*xn(ix1))
+        b = exp(zi*k*xn(ix2))
+        c = exp(zi*k*xn(ix3))
+        psi(ix1,ix2,ix3) = determinant3(a,b,c)
+      end do
+    end do
+  end do
+
+  call antisymmetrize(psi)
+
+end subroutine initialize_eigenstate_guess
+!-------------------------------------------------------
+subroutine orthogonalize_against_states(psi, states, nstates_done)
+  implicit none
+  complex(8),intent(inout) :: psi(0:nx-1,0:nx-1,0:nx-1)
+  complex(8),intent(in) :: states(0:nx-1,0:nx-1,0:nx-1,*)
+  integer,intent(in) :: nstates_done
+  integer :: ipass, jstate
+  complex(8) :: overlap
+
+! Two-pass modified Gram-Schmidt is robust also for nearly degenerate states.
+  do ipass = 1, 2
+    do jstate = 1, nstates_done
+      overlap = inner_product(states(:,:,:,jstate),psi)
+      psi = psi - overlap*states(:,:,:,jstate)
+    end do
+  end do
+  call antisymmetrize(psi)
+
+end subroutine orthogonalize_against_states
+!-------------------------------------------------------
+subroutine minimize_projected_rayleigh(istate, psi, states, nstates_done, &
+    energy, residual_norm)
+  implicit none
+  integer,intent(in) :: istate, nstates_done
+  complex(8),intent(inout) :: psi(0:nx-1,0:nx-1,0:nx-1)
+  complex(8),intent(in) :: states(0:nx-1,0:nx-1,0:nx-1,*)
+  real(8),intent(out) :: energy, residual_norm
+  complex(8),allocatable :: hpsi(:,:,:), residual(:,:,:), xi(:,:,:)
+  complex(8),allocatable :: direction(:,:,:), direction_old(:,:,:), hp(:,:,:)
+  complex(8) :: overlap
+  real(8) :: xixi, xixi_old, gamma, direction_norm, energy_old
+  integer :: iter
+  logical :: converged
+
+  allocate(hpsi(0:nx-1,0:nx-1,0:nx-1))
+  allocate(residual(0:nx-1,0:nx-1,0:nx-1))
+  allocate(xi(0:nx-1,0:nx-1,0:nx-1))
+  allocate(direction(0:nx-1,0:nx-1,0:nx-1))
+  allocate(direction_old(0:nx-1,0:nx-1,0:nx-1))
+  allocate(hp(0:nx-1,0:nx-1,0:nx-1))
+
+  direction_old = (0d0,0d0)
+  xixi_old = 1d0
+  converged = .false.
+
+  call apply_hamiltonian(psi,hpsi,0d0)
+  energy = real(inner_product(psi,hpsi))
+  residual = hpsi - energy*psi
+  residual_norm = wavefunction_norm(residual)
+  write(21,'(2i8,4(1x,1pe20.12))') istate,0,energy,residual_norm, &
+      wavefunction_norm(psi),antisymmetry_error(psi)
+  if (istate == 1) write(20,'(i8,4(1x,1pe20.12))') 0,energy, &
+      residual_norm,wavefunction_norm(psi),antisymmetry_error(psi)
+
+  do iter = 1, cg_max_iter
+    if (residual_norm < cg_residual_tol) then
+      converged = .true.
+      exit
+    end if
+
+    xi = -residual
+    call orthogonalize_against_states(xi,states,nstates_done)
+    overlap = inner_product(psi,xi)
+    xi = xi - overlap*psi
+    call antisymmetrize(xi)
+    xixi = real(inner_product(xi,xi))
+    if (xixi <= 1d-28) exit
+
+! Periodic restart limits loss of conjugacy in nearly degenerate subspaces.
+    if (iter == 1 .or. modulo(iter-1,25) == 0) then
+      gamma = 0d0
+    else
+      gamma = xixi/xixi_old
+    end if
+    direction = xi + gamma*direction_old
+    call orthogonalize_against_states(direction,states,nstates_done)
+    overlap = inner_product(psi,direction)
+    direction = direction - overlap*psi
+    call antisymmetrize(direction)
+    direction_norm = wavefunction_norm(direction)
+    if (direction_norm <= 1d-14) then
+      direction = xi
+      direction_norm = wavefunction_norm(direction)
+      if (direction_norm <= 1d-14) exit
+    end if
+    direction_old = direction
+    direction = direction/direction_norm
+
+    call apply_hamiltonian(direction,hp,0d0)
+    energy_old = energy
+    call rayleigh_ritz_update(psi,direction,hpsi,hp,energy)
+    call orthogonalize_against_states(psi,states,nstates_done)
+    call normalize(psi)
+
+    call apply_hamiltonian(psi,hpsi,0d0)
+    energy = real(inner_product(psi,hpsi))
+    residual = hpsi - energy*psi
+    residual_norm = wavefunction_norm(residual)
+    write(21,'(2i8,4(1x,1pe20.12))') istate,iter,energy,residual_norm, &
+        wavefunction_norm(psi),antisymmetry_error(psi)
+    if (istate == 1) write(20,'(i8,4(1x,1pe20.12))') iter,energy, &
+        residual_norm,wavefunction_norm(psi),antisymmetry_error(psi)
+
+    xixi_old = xixi
+    if (residual_norm < cg_residual_tol .and. &
+        abs(energy-energy_old) < cg_energy_tol) then
+      converged = .true.
+      exit
+    end if
+  end do
+
+  if (.not.converged .and. residual_norm >= cg_residual_tol) then
+    write(*,'(a,i4,a,i8,a,1pe16.8,a,1pe16.8)') &
+        'Warning: eigenstate ',istate,' did not converge in ', &
+        min(iter,cg_max_iter), &
+        ' iterations; energy=',energy,', residual=',residual_norm
+  end if
+
+  deallocate(hpsi,residual,xi,direction,direction_old,hp)
+
+end subroutine minimize_projected_rayleigh
+!-------------------------------------------------------
+subroutine sort_eigenpairs(nstates, values, states, residuals)
+  implicit none
+  integer,intent(in) :: nstates
+  real(8),intent(inout) :: values(nstates), residuals(nstates)
+  complex(8),intent(inout) :: states(0:nx-1,0:nx-1,0:nx-1,nstates)
+  complex(8),allocatable :: work(:,:,:)
+  real(8) :: value_work, residual_work
+  integer :: i, j
+
+  allocate(work(0:nx-1,0:nx-1,0:nx-1))
+  do i = 1, nstates-1
+    do j = i+1, nstates
+      if (values(j) < values(i)) then
+        value_work = values(i)
+        values(i) = values(j)
+        values(j) = value_work
+        residual_work = residuals(i)
+        residuals(i) = residuals(j)
+        residuals(j) = residual_work
+        work = states(:,:,:,i)
+        states(:,:,:,i) = states(:,:,:,j)
+        states(:,:,:,j) = work
+      end if
+    end do
+  end do
+  deallocate(work)
+
+end subroutine sort_eigenpairs
+!-------------------------------------------------------
+subroutine verify_eigenstates(nstates, values, states, residuals)
+  implicit none
+  integer,intent(in) :: nstates
+  real(8),intent(inout) :: values(nstates), residuals(nstates)
+  complex(8),intent(in) :: states(0:nx-1,0:nx-1,0:nx-1,nstates)
+  complex(8),allocatable :: hpsi(:,:,:), residual(:,:,:)
+  complex(8) :: overlap, target
+  real(8) :: max_orth_error
+  integer :: i, j
+
+  allocate(hpsi(0:nx-1,0:nx-1,0:nx-1))
+  allocate(residual(0:nx-1,0:nx-1,0:nx-1))
+  max_orth_error = 0d0
+  do i = 1, nstates
+    do j = 1, nstates
+      target = (0d0,0d0)
+      if (i == j) target = (1d0,0d0)
+      overlap = inner_product(states(:,:,:,i),states(:,:,:,j))
+      max_orth_error = max(max_orth_error,abs(overlap-target))
+    end do
+    call apply_hamiltonian(states(:,:,:,i),hpsi,0d0)
+    values(i) = real(inner_product(states(:,:,:,i),hpsi))
+    residual = hpsi - values(i)*states(:,:,:,i)
+    residuals(i) = wavefunction_norm(residual)
+  end do
+
+  write(*,'(a)') 'Lowest antisymmetric eigenstates:'
+  do i = 1, nstates
+    write(*,'(a,i2,3(a,1pe16.8))') ' state=',i,' energy=',values(i), &
+        ' residual=',residuals(i),' antisymmetry_error=', &
+        antisymmetry_error(states(:,:,:,i))
+  end do
+  write(*,'(a,l2)') ' Energies in ascending order = ', &
+      all(values(2:nstates) >= values(1:nstates-1))
+  write(*,'(a,1pe16.8)') ' Maximum orthonormality error = ',max_orth_error
+
+  deallocate(hpsi,residual)
+
+end subroutine verify_eigenstates
+!-------------------------------------------------------
+subroutine output_eigenstate_currents(nstates, values, states)
+  implicit none
+  integer,intent(in) :: nstates
+  real(8),intent(in) :: values(nstates)
+  complex(8),intent(in) :: states(0:nx-1,0:nx-1,0:nx-1,nstates)
+  real(8) :: current
+  integer :: istate
+
+! The eigenstates belong to the field-free Hamiltonian, so A=0 here.
+  open(22,file='eigenstate_currents.out',status='replace')
+  write(22,'(a)') '# state energy current_expectation_at_A_0'
+  write(*,'(a)') 'Field-free current expectation values:'
+  do istate = 1, nstates
+    current = total_current(states(:,:,:,istate),0d0)
+    write(22,'(i8,2(1x,1pe20.12))') istate,values(istate),current
+    write(*,'(a,i2,2(a,1pe16.8))') ' state=',istate, &
+        ' energy=',values(istate),' current=',current
+  end do
+  close(22)
+
+end subroutine output_eigenstate_currents
 !-------------------------------------------------------
 subroutine rayleigh_ritz_update(psi, p, hpsi, hp, energy)
   implicit none
@@ -489,15 +692,19 @@ end subroutine rayleigh_ritz_update
 !-------------------------------------------------------
 subroutine propagate_tdse
   implicit none
-  integer :: it
-  real(8) :: t, avec
+  integer :: it, istate
+  real(8) :: t, avec, population_sum, max_population_sum
   complex(8),allocatable :: hpsi(:,:,:)
 ! physics
   real(8),allocatable :: current_t(:), energy_t(:), norm_t(:)
+  real(8),allocatable :: population_t(:,:)
+  complex(8),allocatable :: amplitude_t(:,:)
 
   allocate(current_t(0:nt))
   allocate(energy_t(0:nt))
   allocate(norm_t(0:nt))
+  allocate(amplitude_t(num_eigenstates,0:nt))
+  allocate(population_t(num_eigenstates,0:nt))
 
   allocate(hpsi(0:nx-1,0:nx-1,0:nx-1))
 
@@ -510,6 +717,10 @@ subroutine propagate_tdse
     energy_t(it)  = real(inner_product(zpsi, hpsi))
     current_t(it) = total_current(zpsi, avec)
     norm_t(it)    = wavefunction_norm(zpsi)
+    do istate = 1, num_eigenstates
+      amplitude_t(istate,it) = inner_product(eigenvectors(:,:,:,istate),zpsi)
+      population_t(istate,it) = abs(amplitude_t(istate,it))**2
+    end do
 
     if (it < nt) call rk4_step(t, dt)
   end do
@@ -522,7 +733,28 @@ subroutine propagate_tdse
   end do
   close(30)
 
+! Columns: t, followed by Re(amplitude), Im(amplitude), population per state,
+! and finally the population summed over the four-state analysis subspace.
+  open(31,file='state_populations.out',status='replace')
+  max_population_sum = 0d0
+  do it = 0, nt, output_stride
+    t = dble(it)*dt
+    population_sum = sum(population_t(:,it))
+    max_population_sum = max(max_population_sum,population_sum)
+    write(31,"(999e26.16e3)") t, &
+        (real(amplitude_t(istate,it)),aimag(amplitude_t(istate,it)), &
+        population_t(istate,it),istate=1,num_eigenstates),population_sum
+  end do
+  close(31)
+  write(*,'(a,1pe16.8)') 'Maximum four-state population sum = ', &
+      max_population_sum
+  if (maxval(sum(population_t,dim=1)-norm_t**2) > 1d-10) then
+    write(*,'(a,1pe16.8)') 'Warning: population sum exceeds norm squared by ', &
+        maxval(sum(population_t,dim=1)-norm_t**2)
+  end if
+
   deallocate(hpsi)
+  deallocate(amplitude_t,population_t)
 
 end subroutine propagate_tdse
 !-------------------------------------------------------
@@ -670,6 +902,9 @@ subroutine finalize
 
   if (allocated(xn)) deallocate(xn)
   if (allocated(zpsi)) deallocate(zpsi)
+  if (allocated(eigenvalues)) deallocate(eigenvalues)
+  if (allocated(eigenstate_residuals)) deallocate(eigenstate_residuals)
+  if (allocated(eigenvectors)) deallocate(eigenvectors)
   if (allocated(vpot_1d)) deallocate(vpot_1d)
   if (allocated(wpot_1d)) deallocate(wpot_1d)
   if (allocated(vpot)) deallocate(vpot)
